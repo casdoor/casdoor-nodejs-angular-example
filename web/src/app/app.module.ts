5cef2631f09dabce60d02860f1c86a640750a6a7
Yang Luo <hsluoyz@qq.com>
2023-07-29 22:33:12 +0800

    Remove useless code

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
-import { AccountComponent } from './account/account.component'; // Make sure to provide the correct path to the HeaderComponent.
 
 @NgModule({
   declarations: [
